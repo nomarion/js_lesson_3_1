58e2c9db461f277fc3a15b57c144cb16d0ab56cc
nomarion <macmini@MBP-Igor.netis.cc>
2025-06-09 21:07:00 +0300

    Strict

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const array = [7, 21321431242142142432424324n, "Hello World!", true, undefined, null, Symbol("id")];
 for (let i = 0; array.length > i; i++) {
     console.log(String(array[i]) + " : " + typeof array[i]);
